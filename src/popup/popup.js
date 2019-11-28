f860b80353918ffe6fb3a1e35051aded76ace5aa
Son NK <nguyenkims@hotmail.com>
2019-11-28 23:51:16 +0000

    install and init v-clipboard

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import Clipboard from 'v-clipboard'
 
 
 
 global.browser = require('webextension-polyfill')
 Vue.prototype.$browser = global.browser
+
+Vue.use(Clipboard)
+
 
 /* eslint-disable no-new */
 new Vue({
