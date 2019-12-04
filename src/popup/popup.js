e38602183fb8cee7a9f0a3a9dfd3f993866013a4
Son NK <nguyenkims@hotmail.com>
2019-12-04 23:18:40 +0000

    format popup.js

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -7,29 +7,21 @@ import BootstrapVue from 'bootstrap-vue'
 import * as Sentry from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
 
-
-
 global.browser = require('webextension-polyfill')
 Vue.prototype.$browser = global.browser
 
 Vue.use(Clipboard)
-Vue.use(Toasted, {duration: 1000})
+Vue.use(Toasted, { duration: 1000 })
 Vue.use(BootstrapVue)
 
 Sentry.init({
   dsn: 'https://dfc7a7727433452fbe5741b602058cc5@sentry.io/1839562',
-  integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
+  integrations: [new Integrations.Vue({ Vue, attachProps: true, logErrors: true })],
 });
-
-
-
-
 
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  
-  
   render: h => h(App)
 })
