b266ba6aa06e078a8c1e851228d76f5d2982b017
ngxson <thichthat@gmail.com>
2020-07-06 01:37:53 +0200

    add Navigation class

---
+++ src/popup/Navigation.js
@@ -0,0 +1,33 @@
+var router = null;
+
+class Navigation {
+  static PATH = {
+    ROOT: '/',
+    LOGIN: '/login',
+    API_KEY_SETTING: '/api-key-setting',
+    SELF_HOST_SETTING: '/self-host-setting',
+  };
+
+  static getRoutes(components) {
+    return [
+      { path: Navigation.PATH.ROOT, component: components.Login },
+      { path: Navigation.PATH.LOGIN, component: components.Login },
+      { path: Navigation.PATH.API_KEY_SETTING, component: components.ApiKeySetting },
+      { path: Navigation.PATH.SELF_HOST_SETTING, component: components.SelfHostSetting },
+    ];
+  }
+
+  static setRouter($router) {
+    router = $router;
+  }
+
+  static navigateTo(path) {
+    router.push(path);
+  }
+
+  static navigateBack() {
+    // TODO
+  }
+}
+
+export default Navigation;
