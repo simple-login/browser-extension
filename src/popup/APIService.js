e54d0ccbd7bd8d9a676842d38a3ebd7728ad91e6
ngxson <thichthat@gmail.com>
2020-07-13 17:38:38 +0200

    change handle401Error toast message

--- src/popup/APIService.js
+++ src/popup/APIService.js
@@ -97,7 +97,7 @@ const callAPI = async function (
 };
 
 async function handle401Error() {
-  Utils.showError("Invalid API Key. Please logout and re-setup the API Key");
+  Utils.showError("Authentication error, please login again");
   await SLStorage.remove(SLStorage.SETTINGS.API_KEY);
   EventManager.broadcast(EventManager.EVENT.SETTINGS_CHANGED);
   Navigation.clearHistory();
