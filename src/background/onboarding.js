3c66281f6bc2e3a70f334d3ebfaaa4b263d5dcd7
Son NK <>
2021-01-27 19:55:12 +0100

    fix the onboarding show up on browser restart

--- src/background/onboarding.js
+++ src/background/onboarding.js
@@ -11,25 +11,27 @@ function initService() {
       await browser.tabs.create({
         url: browser.runtime.getURL("/onboarding/index.html"),
       });
+
+      // listen for post-setup screen
+      browser.cookies.onChanged.addListener(async function (info) {
+        const { removed, cookie } = info;
+        if (!removed && cookie.name === "setup_done") {
+          const currentTab = await browser.tabs.query({
+            active: true,
+            currentWindow: true,
+          });
+
+          await delay(100);
+
+          await browser.tabs.update(currentTab[0].id, {
+            url: browser.runtime.getURL("/onboarding/index.html#step3"),
+          });
+        }
+      });
     }
   });
 
-  // listen for post-setup screen
-  browser.cookies.onChanged.addListener(async function (info) {
-    const { removed, cookie } = info;
-    if (!removed && cookie.name === "setup_done") {
-      const currentTab = await browser.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
 
-      await delay(100);
-
-      await browser.tabs.update(currentTab[0].id, {
-        url: browser.runtime.getURL("/onboarding/index.html#step3"),
-      });
-    }
-  });
 }
 
 export default { initService };
