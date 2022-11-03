142534bcc1cfdc3109bdd37966d49abb1a3bb675
Son NK <nguyenkims@users.noreply.github.com>
2022-11-03 15:32:06 +0100

    send something else

--- src/popup/components/Main.vue
+++ src/popup/components/Main.vue
@@ -255,6 +255,16 @@ export default {
     };
   },
   async mounted() {
+    try {
+        alert("send 1 message to host app");
+        let r = await browser.runtime.sendNativeMessage("application.id", {
+          message: "1 message",
+        });
+        alert(r);
+      } catch (error) {
+        alert("error" + error);
+      }
+
     this.hostName = await Utils.getHostName();
     this.apiUrl = await SLStorage.get(SLStorage.SETTINGS.API_URL);
     this.apiKey = await SLStorage.get(SLStorage.SETTINGS.API_KEY);
